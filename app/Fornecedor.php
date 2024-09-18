8d22cb3e53d874557cece75089a01e3df4347df2
vandersonfabio <vanderson.fabio@gmail.com>
2024-09-18 15:35:36 -0300

    Iniciando estudos sobre Tinker e Eloquent ORM

--- app/Fornecedor.php
+++ app/Fornecedor.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Fornecedor extends Model
 {
-    //
+    //mudança necessária para que o laravel não procure pela tabela 'fornecedors' (sem o 'E')
+    protected $table = 'fornecedores';
 }
